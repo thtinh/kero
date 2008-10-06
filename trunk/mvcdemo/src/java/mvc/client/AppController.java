12f73880211e214ff019502dc9a5dccb9cfd9484
thtinh <thtinh@4bbac4ce-92bd-11dd-9fdb-9d6513f7d118>
2008-10-06 18:20:25 +0000

    my mvc demo

---
+++ trunk/mvcdemo/src/java/mvc/client/AppController.java
@@ -0,0 +1,63 @@
+/*
+ * Ext GWT - Ext for GWT
+ * Copyright(c) 2007, 2008, Ext JS, LLC.
+ * licensing@extjs.com
+ * 
+ * http://extjs.com/license
+ */
+package mvc.client;
+
+import com.extjs.gxt.ui.client.Registry;
+import com.extjs.gxt.ui.client.mvc.AppEvent;
+import com.extjs.gxt.ui.client.mvc.Controller;
+import com.extjs.gxt.ui.client.mvc.Dispatcher;
+
+public class AppController extends Controller
+{
+
+    private AppView appView;
+
+    public AppController()
+    {
+        registerEventTypes(AppEvents.Init);
+        registerEventTypes(AppEvents.Login);
+        registerEventTypes(AppEvents.Error);
+    }
+
+    public void handleEvent(AppEvent event)
+    {
+        switch (event.type)
+        {
+            case AppEvents.Init:
+                onInit(event);
+                break;
+            case AppEvents.Login:
+                onLogin(event);
+                break;
+            case AppEvents.Error:
+                onError(event);
+                break;
+        }
+    }
+
+    public void initialize()
+    {
+        appView = new AppView(this);
+    }
+
+    protected void onError(AppEvent ae)
+    {
+        System.out.println("error: " + ae.data);
+    }
+
+    private void onInit(AppEvent event)
+    {
+        forwardToView(appView, event);
+       
+    }
+
+    private void onLogin(AppEvent event)
+    {
+        forwardToView(appView, event);
+    }
+}
---
+++ trunk/mvcdemo/src/java/mvc/client/demoEntryPoint.java
@@ -0,0 +1,52 @@
+/*
+ * demoEntryPoint.java
+ *
+ * Created on October 6, 2008, 12:00 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+package mvc.client;
+
+import com.extjs.gxt.ui.client.mvc.Dispatcher;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.button.ButtonBar;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ *
+ * @author Froggy
+ */
+public class demoEntryPoint implements EntryPoint
+{
+
+    /** Creates a new instance of demoEntryPoint */
+    public demoEntryPoint()
+    {
+    }
+
+    /** 
+    The entry point method, called automatically by loading a module
+    that declares an implementing class as an entry-point
+     */
+    public void onModuleLoad()
+    {
+//        ButtonBar bar = new ButtonBar();
+//        Button button = new Button("Click me");
+//        bar.add(button);
+//
+//        RootPanel.get().add(bar);
+        Dispatcher dispatcher = Dispatcher.get();
+        Label label1 = new Label();
+        
+        dispatcher.addController(new AppController());
+        
+        dispatcher.dispatch(AppEvents.Init); 
+        label1.setText(dispatcher.toString());
+        RootPanel.get().add(label1);
+    }
+}
